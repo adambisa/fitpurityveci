0222caaacdf0a73560dc5aa04bf81b57e610b8a1
SIPOS Filip SK79A <Filip.Sipos@erstegroup.com>
2024-03-23 23:50:30 +0100

    de-retardation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Questions from './components/Questions.vue';
-import Header from './components/Header.vue';
+import Questions from "./components/Questions.vue";
+import Header from "./components/Header.vue";
 </script>
 
-<template>  
-    <div class="center">
-      <Header></Header>
-      <Questions></Questions>
-    </div>
+<template>
+  <Header></Header>
+  <Questions></Questions>
 </template>
 
 <style>
-  .center {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-body{
-  background-color:whitesmoke;
+body {
+  background-color: whitesmoke;
 }
 </style>
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -3,7 +3,7 @@ export default {
   data() {
     return {
       checkedNames: [],
-      names:[
+      names: [
         "Zaspal na prednaske?",
         "Vynechal skolu na tyzden?",
         "Prisiek az po polovici prednasky?",
@@ -35,77 +35,96 @@ export default {
         "Odregistroval si predmet po zaciatku semestra?",
         "Odregistrovali mu povinny predmet kvoli kreditovemu stropu?",
         "Bol 1BIT viac ako raz?",
-          ],
+      ],
       submited: false,
-    }
+    };
   },
   methods: {
-    submit(){
-    this.submited = !this.submited
+    submit() {
+      this.submited = !this.submited;
     },
-    clear(){
-      this.checkedNames = []
-    }
-  }
-}
+    clear() {
+      this.checkedNames = [];
+    },
+  },
+};
 </script>
 
 <template>
-<body >
-  <div class="qs" v-if="!submited">
-
-    <ul>
-      <li v-for="(name,index) in names">
-        <input  type="checkbox" v-model="checkedNames" id={{index}} :value="index" />
-        <label for="checkbox"> {{index + 1}}. {{ name }}</label>
-      </li>
-    </ul>
-    <div class="spaced">
-    <a  @click="submit" class="button">submit</a>
-    <a  @click="clear" class="button">clear</a>
+  <body>
+    <div class="qs" v-if="!submited">
+      <ol class="qs__list">
+        <li v-for="(name, index) in names">
+          <input
+            type="checkbox"
+            v-model="checkedNames"
+            :id="index"
+            :value="index"
+            :name="name"
+          />
+          <label :for="index"> {{ name }}</label>
+        </li>
+      </ol>
+      <div class="buttons">
+        <a @click="submit" class="button">Submit</a>
+        <a @click="clear" class="button">Clear</a>
+      </div>
     </div>
-  </div>
 
-  <div v-if="submited" class="finale">
-    Vas fit purity score: {{names.length-checkedNames.length}}/{{ names.length }}
-   </div>
-</body>
+    <div v-if="submited" class="finale">
+      Vas fit purity score: {{ names.length - checkedNames.length }}/{{
+        names.length
+      }}
+    </div>
+  </body>
 </template>
 
-
 <style scoped>
-body{
+@import url("https://fonts.googleapis.com/css2?family=Gabarito:wght@400;500;600;700&family=Teko:wght@300..700&display=swap");
+
+body {
   color: black;
   padding-right: 5;
 }
-.qs{
-  padding-top: 0%;
+
+.qs {
+  width: 100%;
 }
-ul{
-list-style-type: none;
+
+.qs__list {
+  padding-bottom: 1.2rem;
 }
-label{
+
+label {
   padding-left: 1%;
 }
-li input {
-  padding: 5px;
-}
-.finale{
+
+.finale {
   padding-left: 25%;
   font-size: xx-large;
 }
+
+.buttons {
+  display: flex;
+  gap: 0.8rem;
+  padding-bottom: 2.4rem;
+  font-family: "Teko", sans-serif;
+}
+
 .button {
   display: inline-block;
   background-color: #00aae2;
-  color: #ff0027;
-  padding: 15px 32px;
-  font-size: 16px;
+  color: white;
+  padding: 8px 32px;
+  font-size: 1.2rem;
   border-radius: 8px;
+  cursor: pointer;
+  font-weight: 500;
+  letter-spacing: 1px;
 }
-.button:hover{
-  background-color:#ff0027 ;
+.button:hover {
+  background-color: #ff0027;
   color: #00aae2;
   border-radius: 8px;
-
 }
 </style>
