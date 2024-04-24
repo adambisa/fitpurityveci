50b6ac63c5fc1ef8a476c4a6d186e6999edeeb69
Adam Bíša <117039930+adambisa@users.noreply.github.com>
2024-04-24 21:05:27 +0200

    added branding

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Questions from "./components/Questions.vue";
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header></Header>
   <Questions></Questions>
+  <Footer></Footer>
 </template>
 
 <style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dole">
+    <a href="https://ivanovanoha.fit"
+      ><img src="https://ivanovanoha.fit/ivan.png" alt=""
+    /></a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
